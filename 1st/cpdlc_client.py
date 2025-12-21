9d756adec2998b71443bf4a2a2c0e041825bca01
kanf1s <ferdinandovfelix@gmail.com>
2025-12-21 13:02:12 -0300

    Внедрил логирование в этот проект

--- 1st/cpdlc_client.py
+++ 1st/cpdlc_client.py
@@ -1,30 +1,43 @@
 import socket
+from loguru import logger
 
 HOST = 'localhost'
 PORT = 5555
+
+logger.info("Запуск клиента")
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)
 client.connect((HOST, PORT))
 
-print('Соединение с диспетчером установлено')
+logger.info("Соединение с диспетчером установлено")
+print("Соединение с диспетчером установлено")
 
 while True:
     data = client.recv(1024).decode()
-    print('Сообщение от диспетчера:', data)
-    
-    if data.lower() == 'пока':
-        print('Завершение сеанса')
+    logger.info(f"Сообщение от диспетчера: {data}")
+    print("Сообщение от диспетчера:", data)
+
+    if data.lower() == "пока":
+        logger.warning("Диспетчер завершил сеанс")
+        print("Завершение сеанса")
         break
-    if data.lower() == 'прием!':
+
+    if data.lower() == "прием!":
         while True:
-            msg = input('Введите сообщение для диспетчера: ')
-            client.sendall(msg.encode('utf-8'))
-        
-            if msg.lower() == 'пока':
-                print('Вы завершили сеанс')
+            msg = input("Введите сообщение для диспетчера: ")
+            logger.info(f"Отправка диспетчеру: {msg}")
+            client.sendall(msg.encode("utf-8"))
+
+            if msg.lower() == "пока":
+                logger.warning("Пилот завершил сеанс")
+                print("Вы завершили сеанс")
                 break
-            if msg.lower() == 'прием!':
-                print('Ожидание ответа диспетчера...')
+
+            if msg.lower() == "прием!":
+                logger.info("Передача управления диспетчеру")
+                print("Ожидание ответа диспетчера...")
                 break
+
 client.close()
+logger.info("Соединение закрыто")
--- 1st/cpdlc_server.py
+++ 1st/cpdlc_server.py
@@ -1,35 +1,50 @@
 import socket
+from loguru import logger
 
 HOST = 'localhost'
 PORT = 5555
+
+logger.info("Запуск сервера")
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)
 server.bind((HOST, PORT))
 server.listen(1)
 
-print('Ожидание пилота')
+logger.info("Ожидание пилота")
+print("Ожидание пилота")
+
 conn, addr = server.accept()
-print(f'Пилот подключился:', {addr})
+logger.info(f"Пилот подключился: {addr}")
+print(f"Пилот подключился: {addr}")
 
 while True:
-    msg = input('Введите сообщение для пилота: ')
-    conn.sendall(msg.encode('utf-8'))
+    msg = input("Введите сообщение для пилота: ")
+    logger.info(f"Отправка пилоту: {msg}")
+    conn.sendall(msg.encode("utf-8"))
 
-    if msg.lower() == 'пока':
-        print('Завершение сеанса')
+    if msg.lower() == "пока":
+        logger.warning("Диспетчер завершил сеанс")
+        print("Завершение сеанса")
         break
-    
-    elif msg.lower() == 'прием!':
+
+    elif msg.lower() == "прием!":
         while True:
-            print('Ожидание ответа пилота...')
+            logger.info("Ожидание ответа пилота")
+            print("Ожидание ответа пилота...")
             data = conn.recv(1024).decode()
-            print('Ответ пилота:', data)
-            
-            if data.lower() == 'пока':
-                print('Пилот завершили сеанс')
+
+            logger.info(f"Ответ пилота: {data}")
+            print("Ответ пилота:", data)
+
+            if data.lower() == "пока":
+                logger.warning("Пилот завершил сеанс")
+                print("Пилот завершил сеанс")
                 break
-            if data.lower() == 'прием!':
+
+            if data.lower() == "прием!":
+                logger.info("Пилот передал управление диспетчеру")
                 break
 
 conn.close()
+logger.info("Соединение закрыто")
